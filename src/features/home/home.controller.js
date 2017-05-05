9d50001ba17d62998d28f71bb66e11d10e440082
Rodrigo Llanos <rodrigo.llanosordonez@altran.com>
2017-05-05 08:14:16 +0200

    [FEAT] One and two way data binding explanation.

--- src/features/home/home.controller.js
+++ src/features/home/home.controller.js
@@ -1,18 +1,25 @@
-export default function HomeController($state, $log){
+export default function HomeController($state, StorageService) {
     'ngInject';
 
     var vm = this;
 
-    init();
-
+    vm.logout = _logout;
     vm.goTareas = _goTareas;
 
-    function _goTareas(){
+    init();
+
+    function init() {
+        vm.username = StorageService.get('userProfile').username;
+        vm.text = 'Este texto es bindeado desde la home al componente card!'
+    }
+
+    function _logout(){
+        StorageService.remove('userProfile');
+        $state.go('login');
+    }
+
+    function _goTareas() {
         $state.go('tareas');
     }
 
-    function init(){
-        vm.name = 'Rodrigo';
-        vm.text = 'Este texto es bindeado desde la home al componente card!'
-    }
 }
