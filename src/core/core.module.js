857208a7039819acccc780c7408c6119eb84ef6d
Rodrigo Llanos <rodrigo.llanosordonez@altran.com>
2017-05-09 14:01:56 +0200

    [FIX] Error en la importacion de las directivas arreglado.

--- src/core/core.module.js
+++ src/core/core.module.js
@@ -2,7 +2,7 @@ import components from './components/components.module';
 import services from './services/services.module';
 import interceptors from './interceptors/interceptors.module';
 import filters from './filters/filters.module';
-import directives from './filters/filters.module';
+import directives from './directives/directives.module';
 
 angular
     .module('core', [
